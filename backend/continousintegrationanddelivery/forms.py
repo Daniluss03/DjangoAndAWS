17edd2cf707581f8433749454c8ca44fea560502
Danilo <jhondanilo3@gmail.com>
2024-12-04 11:43:58 -0500

    Creation of the login with authentication

--- backend/continousintegrationanddelivery/forms.py
+++ backend/continousintegrationanddelivery/forms.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
+#User login
 
+from  django.contrib.auth.forms import AuthenticationForm  
+
+from django.forms.widgets import PasswordInput,TextInput
+
+from django import forms
 
 class UserForm(UserCreationForm):
     class  Meta:
         model=User
         fields={'username','email','password1','password2'}
        
+
+
+class LoginForm(AuthenticationForm):
+    username=forms.CharField(widget=TextInput()) 
+    password=forms.CharField(widget=PasswordInput())
--- backend/continousintegrationanddelivery/views.py
+++ backend/continousintegrationanddelivery/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render,redirect
 
-from .forms import UserForm
+from .forms import UserForm,LoginForm
+
+from . models import Profile
+
+from django.contrib.auth.models import auth
+
+from django.contrib.auth import authenticate,login
 
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
 
 def index(request):
@@ -15,21 +22,48 @@ def register(request):
     if request.method=='POST':
         form=UserForm(request.POST)
         if form.is_valid():
+            current_user=form.save(commit=False)
             form.save()
-            return redirect('views/login')
+
+            profile=Profile.objects.create(user=current_user)
+            return redirect('login')
         
     context={'form':form}
     return render(request,'register.html',context=context)
     
 
-
-
+@never_cache  
+@login_required(login_url='login')
 def dashboard(request):
     return render(request,'dashboard.html')
 
 
 def login(request):
-    return render(request,'login.html')
+    form=LoginForm()
+    if request.method=='POST':
+        form=LoginForm(request,data=request.POST)
+        if form.is_valid():
+            username=request.POST.get(('username'))
+            password=request.POST.get(('password'))
 
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+                return redirect("dashboard")
+    context={'form':form}
+    return render(request,'login.html',context=context)
+
+
+
+
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect('login')
+    
+
+
+
+@login_required(login_url='login')
 def profile(request):
     return render(request,'profile.html')

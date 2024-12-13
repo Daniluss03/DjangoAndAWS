8424740a690694433f0076a225adc6829cee4547
Danilo <jhondanilo3@gmail.com>
2024-12-12 21:40:08 -0500

    Create rds postgress database and create the connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':config('NAME_DATABASE'),
+        'USER':config('NAME_USER_DATABASE')  ,
+        'PASSWORD':config('PASSWORD_DATABASE') ,
+        'HOST':config('HOST') ,
+        'PORT':config('PORT')
     }
 }
 
